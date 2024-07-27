e2042f1e11f44d054bda64d56f1eea341eb86326
AliHamza <126794067+HamzaNapollo@users.noreply.github.com>
2024-07-26 18:04:52 -0700

    navbar settings

--- client/src/Components/Menu/Sidebar.js
+++ client/src/Components/Menu/Sidebar.js
@@ -9,12 +9,12 @@ const Sidebar = () => {
     return (
         <div class={`hidden md:flex flex-col ${isCollapsed ? 'w-40' : 'w-24'} bg-primaryBlack min-h-screen transition-all duration-200 ease-linear`}>
             <div class={`flex items-center h-[4.8rem] bg-primaryBlack ${isCollapsed ? 'justify-end pr-8' : 'justify-center'}`}>
-                <img src='assets/images/navbar/menu.svg' className='cursor-pointer' alt='menu' onClick={handleCollapse} width={30} />
+                <img src='assets/images/svgs/navbar/menu.svg' className='cursor-pointer' alt='menu' onClick={handleCollapse} width={30} />
             </div>
             <div class="flex flex-col">
                 <nav class="flex-1 px-2 py-4 bg-primaryBlack flex flex-col gap-5">
                     <a href="#" class="flex items-center gap-3 py-2 text-gray-100">
-                        <img src='assets/images/navbar/venu.svg' className='ps-5' alt='' />
+                        <img src='assets/images/svgs/navbar/venu.svg' className='ps-5' alt='' />
                         {isCollapsed && (
                             <p className={`transition-opacity duration-300 ease-linear overflow-hidden ${isCollapsed ? 'opacity-100' : 'opacity-0'}`}>
                                 Lorem
@@ -22,7 +22,7 @@ const Sidebar = () => {
                         )}
                     </a>
                     <a href="#" class=" flex items-center gap-3 py-2 mt-2 text-gray-100">
-                        <img src='assets/images/navbar/home.svg' className='ps-5' alt='' />
+                        <img src='assets/images/svgs/navbar/home.svg' className='ps-5' alt='' />
                         {isCollapsed && (
                             <p className={`transition-opacity duration-300 ease-linear overflow-hidden ${isCollapsed ? 'opacity-100' : 'opacity-0'}`}>
                                 Lorem
@@ -30,7 +30,7 @@ const Sidebar = () => {
                         )}
                     </a>
                     <a href="#" class={`flex items-center gap-3 py-2 mt-2 text-gray-100`}>
-                        <img src='assets/images/navbar/stadium.svg' className='ps-5' alt='' />
+                        <img src='assets/images/svgs/navbar/stadium.svg' className='ps-5' alt='' />
                         {isCollapsed && (
                             <p className={`transition-opacity duration-300 ease-linear overflow-hidden ${isCollapsed ? 'opacity-100' : 'opacity-0'}`}>
                                 Morem
