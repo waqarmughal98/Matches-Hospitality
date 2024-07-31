0aca49daf10ede999d2e12028ff24f29b2c6449a
CodewithAn <codewithan@gmail.com>
2024-08-01 04:46:08 +0500

    changes

--- client/src/Components/UiElements/Loader.js
+++ client/src/Components/UiElements/Loader.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import '../../App.css'
 const Loader = () => {
   return (
-    <div>Loading..</div>
+    <div className='w-full min-h-screen -mt-36 flex justify-center items-center'>
+        <div className="spinner-container flex flex-col gap-3 justify-center items-center">
+           <div className="loading-spinner"></div>
+           <h3 className='text-white'>Fetching Data...</h3>
+
+        </div>
+    </div>
   )
 }
 
