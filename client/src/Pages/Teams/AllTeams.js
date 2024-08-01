42902d8169d438a3b4b932473338078eb57395b8
AliHamza <alihamza.dev4@gmail.com>
2024-07-31 20:34:01 -0700

    team screens

--- client/src/Pages/Teams/AllTeams.js
+++ client/src/Pages/Teams/AllTeams.js
@@ -6,6 +6,7 @@ import CreateTeamModal from '../../Components/Modal/Team/CreateTeamModal';
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import EditTeamModal from '../../Components/Modal/Team/EditTeamModal';
+import DeleteModal from '../../Components/Modal/DeleteModal';
 const AllTeams = () => {
   const { showBackdropWithContent, closeModal } = useAppContext()
 
@@ -76,6 +77,12 @@ const AllTeams = () => {
     )
     showBackdropWithContent(content)
   }
+  const handleBackdrop = () => {
+    const content = (
+      <DeleteModal />
+    )
+    showBackdropWithContent(content)
+  }
 
   return (
     <div className='grid grid-cols-12 gap-y-10'>
@@ -90,8 +97,8 @@ const AllTeams = () => {
       <div className='col-span-12'>
         <div className='grid grid-cols-12 gap-5'>
           {categoryData.map((item, index) => (
-            <div key={index} className='group relative grid xl:col-span-3 md:col-span-12 col-span-12 items-center min-h-48 rounded-xl bg-cover bg-center border-primaryBorder border-[1px]'>
-              <div className='flex text-white gap-5 items-center p-6'>
+            <div key={index} className='group relative grid xl:col-span-3 md:col-span-12 col-span-12 items-center min-h-44 rounded-xl bg-cover bg-center border-primaryBorder border-[1px]'>
+              <div className='flex text-white gap-5 items-center justify-center'>
                 <img className='flex-shrink-0' src={item.logo} alt='logo' width={70} />
                 <div className='flex flex-col gap-y-3'>
                   <div className='flex flex-col'>
@@ -100,10 +107,10 @@ const AllTeams = () => {
                   </div>
                   <div className='flex items-center gap-x-3 py-1 rounded-md text-xs w-fit'>
                     <div className='flex gap-x-3 items-center border-borderInput border px-2 py-1 rounded-md cursor-pointer' onClick={handleShowBackdrop}>
-                    Edit <FaEdit/>
+                      Edit <FaEdit />
                     </div>
-                    <div className='flex gap-x-1 items-center text-black bg-primaryGreen px-2 py-1 rounded-md cursor-pointer font-semibold'>
-                    Delete <AiFillDelete className='mt-[1px]'/>
+                    <div onClick={handleBackdrop} className='flex gap-x-1  items-center text-black bg-primaryGreen px-2 py-1 rounded-md cursor-pointer font-semibold'>
+                      Delete <AiFillDelete className='mt-[1px]' />
                     </div>
                   </div>
                 </div>
