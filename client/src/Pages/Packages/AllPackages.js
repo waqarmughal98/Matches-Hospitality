5b74c6d1b73f7e594fdff4fc2d434e9c8062d2d3
AliHamza <alihamza.dev4@gmail.com>
2024-08-02 19:06:58 -0700

    all packages changes

--- client/src/Pages/Packages/AllPackages.js
+++ client/src/Pages/Packages/AllPackages.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { PrimaryButton } from '../../Components/UiElements/Buttons';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { MdOutlineEdit } from 'react-icons/md';
 const AllPackages = () => {
 
     const packagesData = [
@@ -21,8 +23,8 @@ const AllPackages = () => {
                             <div className='col-span-8 text-4xl font-semibold'>
                                 All Packages
                             </div>
-                            <div className='col-span-4'>
-                                <PrimaryButton size='large' className='font-semibold'>
+                            <div className='grid col-span-4 justify-end'>
+                                <PrimaryButton size='medium' className='font-semibold'>
                                     Create New Package
                                 </PrimaryButton>
                             </div>
@@ -34,7 +36,6 @@ const AllPackages = () => {
                                 <div className='grid grid-cols-12 lg:gap-x-5 gap-y-5'>
                                     <div className='grid lg:col-span-5 col-span-12 bg-white text-primaryBlack rounded-2xl p-6 min-h-[19rem]'>
                                         <div className='flex flex-col'>
-                                            <PrimaryButton size='small' className='self-end'>Popular</PrimaryButton>
                                             <div className='flex flex-col lg:gap-x-8 gap-y-8'>
                                                 <div className='flex flex-col gap-3'>
                                                     <h1 className='text-3xl font-bold'>
@@ -46,8 +47,16 @@ const AllPackages = () => {
                                                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                     </p>
                                                 </div>
-                                                <div className='flex-1'>
+                                                <div className='flex-1 flex justify-between items-center'>
                                                     <PrimaryButton size='medium'>View Details</PrimaryButton>
+                                                    <div className='flex justify-end gap-x-4'>
+                                                        <div className='bg-primaryGreen h-8 w-8 rounded-md flex justify-center items-center cursor-pointer'>
+                                                            <MdOutlineEdit />
+                                                        </div>
+                                                        <div className='bg-primaryGreen h-8 w-8 rounded-md flex justify-center items-center cursor-pointer'>
+                                                            <RiDeleteBinLine />
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -64,10 +73,18 @@ const AllPackages = () => {
                                                             <p className='text-[#464646] text-sm'>
                                                                 {item.desc}
                                                             </p>
-                                                            <div>
+                                                            <div className='flex justify-between items-center'>
                                                                 <PrimaryButton size='medium' color='black'>
                                                                     View Details
                                                                 </PrimaryButton>
+                                                                <div className='flex justify-end gap-x-4'>
+                                                                    <div className='bg-primaryBlack h-8 w-8 rounded-md flex justify-center items-center cursor-pointer'>
+                                                                        <MdOutlineEdit />
+                                                                    </div>
+                                                                    <div className='bg-primaryBlack h-8 w-8 rounded-md flex justify-center items-center cursor-pointer'>
+                                                                        <RiDeleteBinLine />
+                                                                    </div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
