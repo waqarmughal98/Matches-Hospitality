11a3408d78d25f21a0719329c70d26f92ebf853d
Waqar Ali <120112751+Waqarnapollo@users.noreply.github.com>
2024-08-09 07:31:24 +0500

    CORS

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(cors({
   origin: 'https://matches-hospitality.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
